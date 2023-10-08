fc08f769152b7b20bffeadf610f1d9b97d7b873a
Pol Lessire <lessirepol@gmail.com>
2023-10-08 19:57:50 +0200

    Ajoutez des fichiers projet.

---
+++ UserDB/userDetails.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[userDetails]
+(
+	[userid] INT NOT NULL IDENTITY(1, 1), 
+    [username] NVARCHAR(100) NULL, 
+    [address] NVARCHAR(100) NULL, 
+    [cellnumber] NVARCHAR(50) NULL, 
+    [emailid] NVARCHAR(50) NULL,
+    CONSTRAINT [PK_userdetails] PRIMARY KEY CLUSTERED 
+    (
+        [userid] ASC
+    )
+
+)
